d374469615288b9d70860ce4f481d721896c227b
Parham-S <psaniei@hotmail.com>
2021-09-21 06:42:17 -0400

    added tailwind motion

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,18 @@
 import Layout from '../components/layout'
+import { motion } from 'framer-motion'
 
 export default function Projects() {
   return (
     <Layout>
-      <div>Under Construction</div>
+      <motion.div
+        key="home"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="flex flex-col items-center justify-center"
+      >
+        <div>Under Construction</div>
+      </motion.div>
     </Layout>
   )
 }
